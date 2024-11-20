e90a5b0138bdcc2cec0c1a89412dc52b196996e7
Ameer-Mohammed-neox <ameer.mohammed.neox@gmail.com>
2024-11-20 12:02:46 +0100

    Starting_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,25 @@
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:hive_flutter/hive_flutter.dart';
+//test
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter(); // Initialize Hive
+  await Hive.openBox('counters'); // Open a Hive box
   runApp(const MyApp());
 }
 
+//------------------------------------------------------------------------------
+// Main App Widget
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: const Color(0xFF292929), // Background color
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -36,17 +27,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//------------------------------------------------------------------------------
+// Home Page
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +39,144 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final controller = PageController(initialPage: 0);
 
-  void _incrementCounter() {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: PageView(
+          controller: controller,
+          children: const [
+            DetailPage(date: "2024-22-19"),
+            DetailPage(date: "2024-22-18")
+          ],
+        ));
+  }
+}
+
+//------------------------------------------------------------------------------
+// Detail Page
+class DetailPage extends StatefulWidget {
+  const DetailPage({super.key, required this.date});
+
+  final String date;
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsetsDirectional.fromSTEB(0, 60, 0, 32),
+        child: Column(
+          children: [
+            Text(widget.date,
+                style: const TextStyle(color: Colors.white, fontSize: 48)),
+            TrackingElement(
+                color: const Color(0xffd4da13),
+                iconData: Icons.directions_run,
+                unit: "m",
+                max: 5000,
+                date: widget.date),
+            TrackingElement(
+                color: const Color(0xff379bef),
+                iconData: Icons.water_drop,
+                unit: "ml",
+                max: 3000,
+                date: widget.date),
+            TrackingElement(
+                color: const Color(0xffff5858),
+                iconData: Icons.lunch_dining,
+                unit: "cal",
+                max: 2000,
+                date: widget.date)
+          ],
+        ));
+  }
+}
+
+//------------------------------------------------------------------------------
+// Tracking Element
+class TrackingElement extends StatefulWidget {
+  const TrackingElement({
+    super.key,
+    required this.color,
+    required this.iconData,
+    required this.unit,
+    required this.max,
+    required this.date,
+  });
+
+  final Color color;
+  final IconData iconData;
+  final String unit;
+  final double max;
+  final int steps = 5;
+  final String date;
+
+  String get storageKey => '${date}_${unit}';
+
+  @override
+  State<TrackingElement> createState() => _TrackingElementState();
+}
+
+class _TrackingElementState extends State<TrackingElement> {
+  int _counter = 0;
+  double _progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCounter();
+  }
+
+  Future<void> _loadCounter() async {
+    final box = Hive.box('counters'); // Open the Hive box
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter = box.get(widget.storageKey, defaultValue: 0) as int; // Read from Hive
+      _progress = _counter / widget.max;
     });
+  }
+
+  Future<void> _incrementCounter() async {
+    if (_counter < widget.max) {
+      setState(() {
+        _counter += (widget.max / widget.steps).toInt();
+        _progress = _counter / widget.max;
+      });
+      final box = Hive.box('counters'); // Open the Hive box
+      box.put(widget.storageKey, _counter); // Write to Hive
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return InkWell(
+      onTap: _incrementCounter,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(32, 80, 32, 24),
+            child: Row(
+              children: <Widget>[
+                Icon(widget.iconData, color: Colors.white70, size: 50),
+                Text(
+                  ' $_counter / ${widget.max.toInt()} ${widget.unit}',
+                  style: const TextStyle(color: Colors.white, fontSize: 35), // Text color
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+          LinearProgressIndicator(
+            value: _progress,
+            color: widget.color,
+            backgroundColor: const Color(0x9dffffff),
+            minHeight: 12.0,
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
